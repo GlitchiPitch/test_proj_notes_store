a36fcba11f18ceecae8edf978a6c1b310753d4ef
GlitchiPitch <nikitareutsky@gmail.com>
2024-09-21 11:17:01 +0500

    Create notes router & create_note, get_all_notes endpoints

--- core/main.py
+++ core/main.py
@@ -4,6 +4,7 @@ import uvicorn
 from fastapi import FastAPI
 
 from core.database import create_database, delete_database, close_db_connection
+from core.notes_router import router as notes_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -15,6 +16,8 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     lifespan=lifespan,
 )
+
+app.include_router(notes_router)
 
 if __name__ == '__main__':
     uvicorn.run(app)
---
+++ core/notes_router.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy import select
+from core.auth import get_current_user
+from core.database import session_getter
+from core.models import User, Note
+from core.schemas import NoteCreate
+from typing import List
+
+from core.speller import check_text
+
+router = APIRouter(
+    prefix='/notes'
+)
+
+@router.get('/get_all', response_model=List[Note])
+async def get_all_notes(
+        session = Depends(session_getter),
+        current_user: User = Depends(get_current_user),
+):
+    statement = select(Note).where(Note.owner_id == current_user.id)
+    result = await session.execute(statement)
+    notes = result.scalars().all()
+    return notes
+
+@router.post('/create', response_model=Note)
+async def create_note(
+        note_data: NoteCreate,
+        session = Depends(session_getter),
+        current_user: User = Depends(get_current_user),
+):
+    note = Note(
+        title=check_text(note_data.title),
+        description=check_text(note_data.description),
+        owner_id=current_user.id,
+    )
+    session.add(note)
+    await session.commit()
+    await session.refresh(note)
+    return note
