0f3802627ed1ed8cdbd3df06c8d6f958ff9715ae
GlitchiPitch <nikitareutsky@gmail.com>
2024-09-21 11:09:46 +0500

    Create database & add a lifespan for the creating and dropping tables

---
+++ core/database.py
@@ -0,0 +1,32 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+
+from core.models import Base
+
+engine = create_async_engine(
+    url="postgresql+asyncpg://user:pass@localhost:5432/notes",
+    echo=True
+)
+
+SessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+async def session_getter():
+    async with SessionLocal() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
+
+async def close_db_connection():
+    await engine.dispose()
+
+async def create_database():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+async def delete_database():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
--- core/main.py
+++ core/main.py
@@ -1,7 +1,20 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI()
+from core.database import create_database, delete_database, close_db_connection
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_database()
+    yield
+    await delete_database()
+    await close_db_connection()
+
+app = FastAPI(
+    lifespan=lifespan,
+)
 
 if __name__ == '__main__':
     uvicorn.run(app)
